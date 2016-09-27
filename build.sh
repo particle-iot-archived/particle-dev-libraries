cc2bdc5a9a1101f5ac991b2dcbec9a3a230ef628
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-09-27 16:51:44 +0100

    atom build script calls exit, so run it in a subshell to ensure the remainder of our build script executes.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+(
 mv spec spec.safe
 mkdir spec
 cp spec.safe/runner.js spec/
@@ -7,5 +8,6 @@ rm -rf spec
 mv spec.safe spec
 "${APM_SCRIPT_PATH}" install .
 "${APM_SCRIPT_PATH}" link .
+)
 npm test
 
