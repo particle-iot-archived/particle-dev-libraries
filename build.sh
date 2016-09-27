8f4d07a5dc8c028f1b1fc9d280098d47f7d9f1be
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-09-27 18:40:17 +0100

    even when sourcing the script, it causes an exit (so we cannot use the environment variables set). Remove the last line, which removes the call to exit.

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ mv spec spec.safe
 mkdir spec
 cp spec.safe/runner.js spec/
 curl -s https://raw.githubusercontent.com/atom/ci/master/build-package.sh > atombuild.sh
+# remove the exit command at the end
+sed -i '$ d' atombuild.sh
 source atombuild.sh
 rm -rf spec
 mv spec.safe spec
