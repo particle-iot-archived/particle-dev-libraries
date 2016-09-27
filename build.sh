944c651b50575ffaddaaa93471ddd83353fca399
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-09-27 18:53:03 +0100

    another try

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ mkdir spec
 cp spec.safe/runner.js spec/
 curl -s https://raw.githubusercontent.com/atom/ci/master/build-package.sh > atombuild.sh
 # remove the exit command at the end
-source `grep -v -e  "exit$" atombuild.sh`  
+grep -v -e  "exit$" atombuild.sh > noexit.sh
+source noexit.sh  
 rm -rf spec
 mv spec.safe spec
 "${APM_SCRIPT_PATH}" install .
