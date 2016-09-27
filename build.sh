49d4cd343def9e5ae6f4452eb3ff8e6d71cb9eb6
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-09-27 18:57:04 +0100

    with exit removed, no need for a subshell

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-(
 mv spec spec.safe
 mkdir spec
 cp spec.safe/runner.js spec/
@@ -10,8 +9,5 @@ rm -rf spec
 mv spec.safe spec
 "${APM_SCRIPT_PATH}" install .
 "${APM_SCRIPT_PATH}" link .
-echo "export NPM_SCRIPT_PATH=$(NPM_SCRIPT_PATH)" > ~/npm_script
-)
-source ~/npm_script
 "$(NPM_SCRIPT_PATH)" test
 
