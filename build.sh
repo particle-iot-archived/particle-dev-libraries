978e286b249e5fd48e5dcf92afb87704055360ae
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-09-28 11:14:12 +0100

    write access token to profile

--- build.sh
+++ build.sh
@@ -7,6 +7,10 @@ grep -v -e  "exit$" atombuild.sh > noexit.sh
 source noexit.sh  
 rm -rf spec
 mv spec.safe spec
+mkdir ~/.particle
+echo '{"name":"particle"}' > ~/.particle/profile.json
+echo $PARTICLE_CONFIG_JSON > ~/.particle/particle.config.json
+
 "${APM_SCRIPT_PATH}" install .
 "${APM_SCRIPT_PATH}" link .
 "${NPM_SCRIPT_PATH}" run test:unit
