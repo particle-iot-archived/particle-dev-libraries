75e0634fecc835fda831f4289f792d0937c67f81
Wojtek Siudzinski <admin@suda.pl>
2016-10-19 19:00:48 +0100

    Rename Publish to Contribute

--- spec/library_contribute.spec.js
+++ spec/library_contribute.spec.js
@@ -3,6 +3,7 @@
 import path from 'path';
 import {copySync} from 'fs-extra';
 import {expect} from 'chai';
+import when from 'when';
 import {expectNotification} from './package.spec';
 import {projectSelectedScope} from './project.spec';
 import {runCommand} from './commands.spec';
@@ -53,14 +54,14 @@ projectSelectedScope((context) => {
 
 	/* unused
 	function expectNoLibraryProjectNotificationIsShown() {
-		const module = require('../lib/library_publish');
+		const module = require('../lib/library_contribute');
 		const expected = module.noLibraryProjectSelectedNotification();
 		expectNotification(expected);
 	}
 	*/
 
 	function expectValidationErrorNotification(msg) {
-		const module = require('../lib/library_publish');
+		const module = require('../lib/library_contribute');
 		const expected = module.notifyValidationError(msg);
 		expectNotification(expected);
 	}
@@ -73,20 +74,14 @@ projectSelectedScope((context) => {
 		return expectValidationErrorNotification('Library is not valid. library must be migrated from v1 format');
 	}
 
-	function runPublish(then) {
-		return runCommand('publish', then);
+	function runContribute(then) {
+		return runCommand('contribute', then);
 	}
 
-	function expectPublishLibrary(notification) {
-		return runPublish(() => {
-			expectNotification(notification);
-		});
-	}
-
-	describe('and library publish is run in a directory', () => {
+	describe('and library contribute is run in a directory', () => {
 		describe('that is not a library', () => {
 			it('then displays an error that the current project isn\'t a library', () => {
-				return runPublish(() => {
+				return runContribute(() => {
 					expectLibraryIsNotValidNotificationIsShown();
 				});
 			});
@@ -99,7 +94,7 @@ projectSelectedScope((context) => {
 			});
 
 			it('then displays an error that the current project isn\'t a library', () => {
-				return runPublish(() => {
+				return runContribute(() => {
 					expectLibraryMustBeMigratedFromV1Notification();
 				});
 			});
@@ -116,26 +111,32 @@ projectSelectedScope((context) => {
 				return libraryDelete(libraryName);
 			});
 
-			it('can publish the library', () => {
+			it('can contribute the library', () => {
 				const version = '0.0.1';
 				copySync(path.join(resourcesDirectory(), 'libraries', 'publish', 'valid', version), context.projectDir);
-				const module = require('../lib/library_publish');
-				const publish = expectPublishLibrary(module.notifyLibraryPublished({name:libraryName, metadata: {version}}));
-				return publish;
+				const module = require('../lib/library_contribute');
+				return runContribute(() => {
+					return expectNotification(module.notifyLibraryValidating(context.projectDir + '/'));
+				}).then(() => {
+					return expectNotification(module.notifyLibraryContributed({name:libraryName, metadata: {version}}));
+				});
 			});
 
-			it('cannot republish the same version of the library', () => {
+			it('cannot recontribute the same version of the library', () => {
 				const version = '0.0.1';
 				copySync(path.join(resourcesDirectory(), 'libraries', 'publish', 'valid', version), context.projectDir);
-				return expect(runPublish(() => {})).to.eventually.be.rejectedWith('This version already exists. Version must be greater than 0.0.1');
+				return expect(runContribute(() => {})).to.eventually.be.rejectedWith('This version already exists. Version must be greater than 0.0.1');
 			});
 
-			it('can publish the library with a new version', () => {
+			it('can contribute the library with a new version', () => {
 				const version = '0.0.2';
 				copySync(path.join(resourcesDirectory(), 'libraries', 'publish', 'valid', version), context.projectDir);
-				const module = require('../lib/library_publish');
-				const publish = expectPublishLibrary(module.notifyLibraryPublished({name:libraryName, metadata: {version}}));
-				return publish;
+				const module = require('../lib/library_contribute');
+				return runContribute(() => {
+					return expectNotification(module.notifyLibraryValidating(context.projectDir + '/'));
+				}).then(() => {
+					return expectNotification(module.notifyLibraryContributed({name:libraryName, metadata: {version}}));
+				});
 			});
 
 			it('can delete the existing library as cleanup', () => {
