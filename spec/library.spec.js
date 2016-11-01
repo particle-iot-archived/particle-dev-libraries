64be1b14c792ca3e01020005dab57dba7a1ff0c4
Wojtek Siudzinski <admin@suda.pl>
2016-11-01 15:33:54 +0000

    Fix linter errors

--- spec/library.spec.js
+++ spec/library.spec.js
@@ -4,39 +4,39 @@ import {fetchApiClient} from '../lib/library';
 const {LibraryDeleteCommandSite, LibraryDeleteCommand} = require('../lib/cli');
 
 export class DevLibraryDeleteCommandSite extends LibraryDeleteCommandSite {
-    constructor(apiClient, name) {
-        super();
-        this._apiClient = apiClient;
-        this.name = name;
-    }
+	constructor(apiClient, name) {
+		super();
+		this._apiClient = apiClient;
+		this.name = name;
+	}
 
-    apiClient() {
-        return this._apiClient;
-    }
+	apiClient() {
+		return this._apiClient;
+	}
 
-    libraryIdent() {
-        return this.name;
-    }
+	libraryIdent() {
+		return this.name;
+	}
 
-    /**
-     * Notifies the site that the command is about to retrieve the libraries.
-     * @param {Promise} promise   The command to retrieve the libraries.
-     * @param {string} libraryIdent     The identifier of the library/version
-     * @return {Promise} to list libraries
-     */
-    notifyStart(promise, libraryIdent) {
-        return promise;
-    }
+	/**
+	 * Notifies the site that the command is about to retrieve the libraries.
+	 * @param {Promise} promise	 The command to retrieve the libraries.
+	 * @param {string} libraryIdent		 The identifier of the library/version
+	 * @return {Promise} to list libraries
+	 */
+	notifyStart(promise, libraryIdent) {
+		return promise;
+	}
 
-    notifyComplete(promise, result, error) {
-        if (error) {
-            throw error;
-        }
-    }
+	notifyComplete(promise, result, error) {
+		if (error) {
+			throw error;
+		}
+	}
 }
 
 export function libraryDelete(name) {
-    const apiClient = fetchApiClient();
-    const command = new LibraryDeleteCommand();
-    return new DevLibraryDeleteCommandSite(apiClient, name).run(command, {});
+	const apiClient = fetchApiClient();
+	const command = new LibraryDeleteCommand();
+	return new DevLibraryDeleteCommandSite(apiClient, name).run(command, {});
 }
--- spec/library_publish.spec.js
+++ spec/library_publish.spec.js
@@ -9,44 +9,44 @@ import {runCommand} from './commands.spec';
 import {libraryDelete} from './library.spec';
 
 projectSelectedScope((context) => {
-    function expectValidationErrorNotification(msg) {
-        const module = require('../lib/library_contribute');
-        const expected = module.notifyValidationError(msg);
-        expectNotification(expected);
-    }
+	function expectValidationErrorNotification(msg) {
+		const module = require('../lib/library_contribute');
+		const expected = module.notifyValidationError(msg);
+		expectNotification(expected);
+	}
 
-    function runPublish(then) {
-        return runCommand('publish', then);
-    }
+	function runPublish(then) {
+		return runCommand('publish', then);
+	}
 
-    describe('and library publish is run in a directory', () => {
-        describe('that is a v2 library', () => {
-            const libraryName = 'test-library-publish';
-            let resourcesDirectory;
-            before(() => {
-                resourcesDirectory = require('particle-library-manager').resourcesDir;
-            });
+	describe('and library publish is run in a directory', () => {
+		describe('that is a v2 library', () => {
+			const libraryName = 'test-library-publish';
+			let resourcesDirectory;
+			before(() => {
+				resourcesDirectory = require('particle-library-manager').resourcesDir;
+			});
 
-            it('can delete the existing library', () => {
-                return libraryDelete(libraryName);
-            });
+			it('can delete the existing library', () => {
+				return libraryDelete(libraryName);
+			});
 
-            it('can publish the library', () => {
-                const version = '0.0.1';
-                copySync(path.join(resourcesDirectory(), 'libraries', 'contribute', 'valid', version), context.projectDir);
-                const module = require('../lib/library_publish');
-                return runCommand('contribute', () => {
-                    console.log('CONTIRBUTE');
-                    return runPublish(() => {
-                        console.log('PUBLISH');
-                        return expectNotification(module.notifyLibraryPublished({name:libraryName, metadata: {version}}));
-                    });
-                });
-            });
+			it('can publish the library', () => {
+				const version = '0.0.1';
+				copySync(path.join(resourcesDirectory(), 'libraries', 'contribute', 'valid', version), context.projectDir);
+				const module = require('../lib/library_publish');
+				return runCommand('contribute', () => {
+					console.log('CONTIRBUTE');
+					return runPublish(() => {
+						console.log('PUBLISH');
+						return expectNotification(module.notifyLibraryPublished({name:libraryName, metadata: {version}}));
+					});
+				});
+			});
 
-            it('can delete the existing library as cleanup', () => {
-                return libraryDelete(libraryName);
-            });
-        });
-    });
+			it('can delete the existing library as cleanup', () => {
+				return libraryDelete(libraryName);
+			});
+		});
+	});
 });
